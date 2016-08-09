f1585040ac69205a6659448346c9ba2465fba50e
James Hopkins <jameshopkins0709@gmail.com>
2016-08-09 18:47:04 +0100

    Isomorphic rendering. Browser HMR doesn't work seemingly due to react router blocking re-render

--- app/containers/homepage.js
+++ app/containers/homepage.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class extends Component {
   render() {
     return (
-      <div>HELLOBRIILL</div>
+      <div>FUL</div>
     );
   }
 }
--- app/main.js
+++ app/main.js
@@ -1,1 +1,10 @@
-console.log('naureh');
+import React from 'react';
+import { render } from 'react-dom';
+import Router from 'react-router';
+import routes from './routes';
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+render(routes, document.getElementById('app'));
