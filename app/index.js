8ed589b110bce18158063a4a1d1d3c61ee860b8d
James Muir-Hopkins <jameshopkins0709@gmail.com>
2016-08-11 16:13:35 +0100

    Add Redux Saga setup with basic POST endpoint in which to trigger SNS messaging

--- app/index.js
+++ app/index.js
@@ -4,13 +4,16 @@ import { Provider } from 'react-redux';
 import Router from 'react-router';
 import routes from './routes';
 import store from './store';
+import saga from './sagas';
+
+store.sagaMiddleware.run(saga);
 
 if (module.hot) {
   module.hot.accept();
 }
 
 render(
-  <Provider store={store}>
+  <Provider store={store.store}>
     {routes}
   </Provider>
   , document.getElementById('app')
