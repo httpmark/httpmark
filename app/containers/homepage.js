860d54ad8b01832357faadbd1304baf4805e5804
James Muir-Hopkins <jameshopkins0709@gmail.com>
2016-08-11 12:34:58 +0100

    Basic Redux implementation

--- app/containers/homepage.js
+++ app/containers/homepage.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import spawnAgent from '../actions/agent';
 
-export default class extends Component {
+class Homepage extends Component {
   render() {
     return (
-      <div>FUL</div>
+      <div>
+        <button onClick={this.props.hello}>Launch agent</button>
+        {this.props.agent && 'Agent launched!'}
+      </div>
     );
   }
 }
+
+const mapDispatchToProps = dispatch => ({
+  hello: () => {
+    return dispatch(spawnAgent('works'))
+  }
+});
+
+const mapStateToProps = state => ({
+  agent: state.agent
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Homepage);
--- app/main.js
+++ app/main.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import Router from 'react-router';
 import routes from './routes';
+import store from './store';
 
 if (module.hot) {
   module.hot.accept();
 }
 
-render(routes, document.getElementById('app'));
+render(
+  <Provider store={store}>
+    {routes}
+  </Provider>
+  , document.getElementById('app')
+);
