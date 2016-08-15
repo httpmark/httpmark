3db7e40e33cc038bc1d88d913b0412e7b5543b8f
James Hopkins <jameshopkins0709@gmail.com>
2016-08-15 19:13:22 +0100

    Add remote storage of Terraform state into S3

--- infrastructure/init.sh
+++ infrastructure/init.sh
@@ -6,6 +6,15 @@ cd ./tmp/agent_spawn_deploy
 zip ./agent_spawn.zip -r ./index.js
 cd ../..
 
+terraform remote config -backend=s3 \
+  -backend-config="bucket=webapptest-terraform" \
+  -backend-config="access_key=$AWS_ACCESS_KEY_ID" \
+  -backend-config="secret_key=$AWS_SECRET_ACCESS_KEY" \
+  -backend-config="region=eu-west-1" \
+  -backend-config="key=state"
+
+terraform remote pull
 terraform get && terraform $1 \
   -var "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" \
   -var "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY"
+terraform remote push
