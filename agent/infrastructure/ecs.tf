2d2b264db794b701d860b3290f47f36a412fbccc
James Muir-Hopkins <jameshopkins0709@gmail.com>
2016-10-06 23:05:23 +0100

    Tidy up infrastructure deploy tasks

--- agent/infrastructure/ecs.tf
+++ agent/infrastructure/ecs.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region = "eu-west-1"
-}
-
 resource "aws_ecs_cluster" "default" {
   name = "test-agents-${var.environment}"
 }
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,9 +1,12 @@
 provider "aws" {
   region = "eu-west-1"
-  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
-  access_key = "${var.AWS_ACCESS_KEY_ID}"
+  secret_key = "${var.aws_secret_key}"
+  access_key = "${var.aws_access_key}"
 }
 
-module "agent_spawn" {
-  source="./agents/spawn"
+module "test_agent" {
+  source="../agent/infrastructure"
+  aws_secret_key="${var.aws_secret_key}"
+  aws_access_key="${var.aws_access_key}"
+  environment="${var.environment}"
 }
