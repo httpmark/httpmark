4cf28e2674281f811d88ab35275308b568623f63
James Hopkins <jameshopkins0709@gmail.com>
2016-10-07 13:06:23 +0100

    Nicer interface for pushing images to ECR

---
+++ agent/infrastructure/bin/deploy_image.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Logging into ECR..."
+eval `aws ecr get-login`
+
+cd ../../infrastructure
+
+ECR_TAG=`terraform output ecr_repository_url`
+LOCAL_TAG="webapptest_test_agent"
+
+echo "Tagging and pushing tag"
+docker tag $LOCAL_TAG $ECR_TAG
+docker push $ECR_TAG
