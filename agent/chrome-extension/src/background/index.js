0d1e00679214138c6c6dc5d92580deeb162d040f
James Hopkins <jameshopkins0709@gmail.com>
2016-10-14 14:00:00 +0100

    Initial test orchestration using chrome.devtools API
    WebSocket instantiated in the event page sends HAR-formatted metrics back to the Node server

---
+++ agent/chrome-extension/src/background/index.js
@@ -0,0 +1,68 @@
+import 'babel-polyfill';
+import { set, lensProp } from 'ramda';
+
+const portDictionary = new Map();
+
+export const setConnectionMapping = (existingPorts, newPort) => {
+  return (context, tabId) => {
+    const currentValue = existingPorts.get(tabId) || {};
+    existingPorts.set(tabId, set(lensProp(context), newPort, currentValue));
+  }
+};
+
+export const getTestPlan = async (fetchPlan = fetch) => {
+  const response = await fetchPlan('http://127.0.0.1:3000/task');
+  return await response.json();
+}
+
+export const createWSConnection = (cb, WS = WebSocket) => {
+  const connection = new WS('ws://127.0.0.1:3001');
+  connection.onopen = cb(connection);
+};
+
+export const createMessageBus = (devtools, content, WS = WebSocket) => {
+  const registerDevToolsMessageListener = devtools => websocket => {
+    devtools.onMessage.addListener(({ status, payload }) => {
+      if (status === 'HAR-formatted-asset') {
+        websocket.send(JSON.stringify(payload));
+      }
+    })
+  };
+  createWSConnection(registerDevToolsMessageListener(devtools));
+  content.onMessage.addListener(message => devtools.postMessage(message))
+}
+
+export const onConnectListener = (
+  ports = portDictionary,
+  setConnection = setConnectionMapping,
+  tabs = chrome.tabs,
+  testPlan = getTestPlan
+) => {
+  return async port => {
+    const addConnection = setConnection(ports, port);
+    let tabId;
+    if (port.name === 'content') {
+      // Connection from content script
+      tabId = port.sender.tab.id;
+      addConnection('content', tabId);
+    } else {
+      // Connection from DevTools
+      tabId = parseInt(port.name);
+      addConnection('devtools', tabId);
+      const { url } = await testPlan();
+      tabs.update({ url }, () => {
+        tabs.executeScript(tabId, {
+          file: 'build/content.js',
+          runAt: 'document_start'
+        });
+      });
+    }
+
+    const { content, devtools } = ports.get(tabId);
+    if (content && devtools) {
+      createMessageBus(devtools, content);
+    }
+  }
+}
+
+global.chrome.runtime.onConnect.addListener(onConnectListener());
---
+++ agent/chrome-extension/src/background/index.spec.js
@@ -0,0 +1,108 @@
+import {
+  setConnectionMapping,
+  onConnectListener as onConnect,
+  createMessageBus
+} from './';
+
+describe('Event Page', () => {
+  describe('Port initialisation through context-based routing', () => {
+    it('Initialises a new context against a new tab ID', () => {
+      const ports = new Map();
+      const setup = setConnectionMapping(ports, { port: 'devtools wahey!' })
+      const result = setup('devtools', 2);
+      expect(ports.has(2)).to.be.true;
+      expect(ports.get(2)).to.deep.equal({
+        devtools: {
+          port: 'devtools wahey!'
+        }
+      });
+    });
+    it('Appends a new context to an existing tab ID with a context', () => {
+      const ports = new Map();
+      setConnectionMapping(ports, { port: 'devtools yummy' })('devtools', 2);
+      const setup = setConnectionMapping(ports, { port: 'a groovy content script' });
+      const result = setup('content', 2);
+      expect(ports.get(2)).to.deep.equal({
+        devtools: {
+          port: 'devtools yummy'
+        },
+        content: {
+          port: 'a groovy content script'
+        }
+      });
+    });
+  });
+  describe('Registering long-lived channels between content script, DevTools extension, and event page', () => {
+    describe('Registering a context', () => {
+      const tabId = 5;
+      const ports = new Map([
+        [ tabId, [{}] ]
+      ]);
+
+      describe('Content', () => {
+        it('Adds the connection from the context', () => {
+          const bindPort = spy();
+          const setConnectionMapping = () => bindPort;
+          const listener = onConnect(ports, setConnectionMapping);
+          listener({
+            name: 'content',
+            sender: { tab: { id: tabId } }
+          })
+          expect(bindPort).to.have.been.calledWith('content', tabId)
+        });
+      });
+
+      describe('DevTools', () => {
+        const tabs = {
+          update: () => {},
+          executeScript: () => {}
+        }
+        it('Adds the connection from the context', async () => {
+          const bindPort = spy();
+          const setConnectionMapping = () => bindPort;
+          const listener = onConnect(ports, setConnectionMapping, tabs, () => Promise.resolve({ url: 'some-url' }));
+          await listener({
+            name: tabId
+          })
+          expect(bindPort).to.have.been.calledWith('devtools', tabId)
+        });
+
+        it('Requests the test plan', async () => {
+          const testPlan = spy(() => Promise.resolve({ url: 'wahey' }));
+          const setConnectionMapping = () => () => {};
+          const listener = onConnect(ports, setConnectionMapping, tabs, testPlan);
+          await listener({
+            name: tabId
+          });
+          expect(testPlan).to.have.been.called;
+        });
+
+        it('Updates the current tab with the retrieved test URL, and then injects the content script', async () => {
+          const tabUpdateSpy = spy();
+          const tabExeuteScriptSpy = spy();
+          const tabs = {
+            update: tabUpdateSpy,
+            executeScript: tabExeuteScriptSpy
+          }
+          const setConnectionMapping = () => () => {};
+          const listener = onConnect(ports, setConnectionMapping, tabs, () => Promise.resolve({ url: 'some-url' }));
+          await listener({
+            name: tabId
+          });
+          expect(tabUpdateSpy).to.have.been.calledWith({ url: 'some-url' });
+          tabUpdateSpy.args[0][1]();
+          expect(tabExeuteScriptSpy).to.have.been.calledWith(5, {
+            file: 'build/content.js',
+            runAt: 'document_start'
+          });
+        });
+      });
+    });
+
+    describe('Message Bus Creation', () => {
+      it('works', () => {
+        const WebSocket = spy();
+      })
+    })
+  });
+})
