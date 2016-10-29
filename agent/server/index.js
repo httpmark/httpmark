7aec08476b3798faea921d9dd00ca6a4ae544c3f
James Hopkins <jameshopkins0709@gmail.com>
2016-10-29 14:42:36 +0100

    Move server src into a more logical place

--- agent/server/index.js
+++ agent/server/index.js
@@ -8,11 +8,13 @@ import rimraf from 'rimraf';
 import { sync as whichSync } from 'which';
 import bodyParser from 'body-parser';
 import { Server as WebSocketServer } from 'ws';
+import createHARBlob from './har';
 
 const wss = new WebSocketServer({ port: 3001 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
+    ws.send(createHARBlob(message))
     console.log('received: %s', message);
   });
 });
