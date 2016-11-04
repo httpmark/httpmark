ee89369eb499c4dd7901df27eea277b58813cce2
Jon Sharratt <jon@craftship.io>
2016-11-04 15:45:20 +0000

    Fix linting issues in index.js

--- app/server/index.js
+++ app/server/index.js
@@ -1,52 +1,61 @@
 import { createServer } from 'http';
-import { Server as WebSocketServer } from 'ws';
 import net from 'net';
 import express from 'express';
 import { Lambda } from 'aws-sdk';
 import bodyParser from 'body-parser';
 
-const server = createServer();
+const log = (...args) =>
+  console.log(...args); // eslint-disable-line no-console
 
-const wss = new WebSocketServer({ port: 3002 });
+const server = createServer();
 const tcpServer = net.createServer();
 
 tcpServer.on('connection', conn => {
-  const remoteAddress = conn.remoteAddress + ':' + conn.remotePort;
+  const remoteAddress = `${conn.remoteAddress}:${conn.remotePort}`;
+
   conn.setEncoding('utf8');
+
   conn.on('data', d => {
-    console.log('connection data from %s: %j', remoteAddress, d);
+    log('connection data from %s: %j', remoteAddress, d);
     conn.write(d);
   });
+
   conn.once('close', () => {
-    console.log('connection from %s closed', remoteAddress);
+    log('connection from %s closed', remoteAddress);
   });
+
   conn.on('error', err => {
-    console.log('Connection %s error: %s', remoteAddress, err.message);
+    log('Connection %s error: %s', remoteAddress, err.message);
   });
 });
 
 tcpServer.listen(9000, () => {
-  console.log('server listening to %j', tcpServer.address());
+  log('server listening to %j', tcpServer.address());
 });
 
 const lambdaClient = new Lambda({
   region: 'eu-west-1',
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
 const app = express();
+
 const port = 3000;
 
 app.use(bodyParser.json());
 
 app.use(express.static('ui'));
 
-app.get('/', (req, res, next) => {
-  res.sendFile('./ui/build/index.html', { root: process.cwd() });
+app.get('/', (_, res) => {
+  res.sendFile(
+    './ui/build/index.html', {
+      root: process.cwd()
+    }
+  );
 });
 
-app.get('/api', (req, res, next) => {
+app.get('/api', (req, res) => {
   res.json(req.query);
 });
 
@@ -56,21 +65,22 @@ app.post('/spawn-agent', (req, res) => {
     Payload: JSON.stringify({
       url: 'some URL!!',
       tcpHost: process.env.TCP_HOST,
-      tcpPort: process.env.TCP_PORT,
-    }),
-  }, (err, data) => {
+      tcpPort: process.env.TCP_PORT
+    })
+  }, (err) => {
+    log(err);
     res.sendStatus(200);
   });
 });
 
 app.use((err) => {
-  console.log(err);
+  log(err);
 });
 
 server.on('request', app);
 
 server.listen(port, () => {
-  console.log(`App server listening on port ${port}`);
+  log(`App server listening on port ${port}`);
 });
 
 export default app;
